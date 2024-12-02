5dae9580a99213549a36238219fb64a21d2a6ca6
younguk <0622ook@gmail.com>
2024-12-02 12:31:46 +0900

    add3

--- src/app/addTopic/page.tsx
+++ src/app/addTopic/page.tsx
@@ -1,5 +1,6 @@
-<<<<<<< HEAD
+
 'use client'
+import AddTopicForm from '@/components/AddTopicForm'
 import { useSession } from 'next-auth/react'
 import { redirect, useRouter } from 'next/navigation'
 import React, { useState } from 'react'
@@ -9,13 +10,14 @@ export default function AddTopic() {
    const [description, setDescription] = useState('')
 
    const router = useRouter()
-   const {data:session} = useSession()
+   const { data: session } = useSession()
 
-   if (!session){
+   if (!session) {
       redirect(
          ('/login')
       )
    }
+
 
    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault()
@@ -43,11 +45,6 @@ export default function AddTopic() {
          console.log(error)
       }
    }
-=======
-import AddTopicForm from '@/components/AddTopicForm'
->>>>>>> a7e50e1e7df88c03e02e57bfe6331ba951dd003c
-
-export default function AddTopicPage() {
    return (
       <div className="max-w-3xl mx-auto mt-8">
          <h1 className="text-2xl font-bold mb-4">새 토픽 추가</h1>
