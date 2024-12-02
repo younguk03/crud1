52f38955cadc5563d16fb8efb907350b4a717077
younguk <0622ook@gmail.com>
2024-12-02 13:19:04 +0900

    edit

--- src/app/addTopic/page.tsx
+++ src/app/addTopic/page.tsx
@@ -1,50 +1,6 @@
-
-'use client'
-import AddTopicForm from '@/components/AddTopicForm'
-import { useSession } from 'next-auth/react'
-import { redirect, useRouter } from 'next/navigation'
-import React, { useState } from 'react'
+import AddTopicForm from "@/components/AddTopicForm";
 
 export default function AddTopic() {
-   const [title, setTitle] = useState('')
-   const [description, setDescription] = useState('')
-
-   const router = useRouter()
-   const { data: session } = useSession()
-
-   if (!session) {
-      redirect(
-         ('/login')
-      )
-   }
-
-
-   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault()
-
-      if (!title || !description) {
-         alert('Title and description are required.')
-      }
-
-      try {
-         const res = await fetch('/api/topics', {
-            method: 'POST',
-            headers: {
-               'Content-type': 'application/json',
-            },
-            body: JSON.stringify({ title, description }),
-         })
-
-         if (res.ok) {
-            router.push('/')
-            router.refresh()
-         } else {
-            throw new Error('Failed to create a topic')
-         }
-      } catch (error) {
-         console.log(error)
-      }
-   }
    return (
       <div className="max-w-3xl mx-auto mt-8">
          <h1 className="text-2xl font-bold mb-4">새 토픽 추가</h1>
